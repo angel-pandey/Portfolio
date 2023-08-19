46991b2f918dad3c1ab80d7e75a07b7d349f8e91
angel-pandey <angelpandey2014@gmail.com>
2023-08-19 19:43:50 +1000

    Modified the Thankyou.html to thankyou.php made it better.

---
+++ thankyou.php
@@ -0,0 +1,23 @@
+<?php
+$title = "Thank You";
+include "header.php";
+?>
+  
+        <h1 style="text-align:center"> Thank You! </h1>
+        <div class="project-container" >
+        <div  class="project-card">
+          
+          <h3 style="text-align: center">!!  Thank you for Reaching Out  !!</h3>
+          <p class="subtext">
+            We have received your message.</br>
+            We will get back to you as soon as possible.</br>
+            We will handle all the communication based on our <a target="_blank" style="color:#FF4057 !important" href="privacy.php"> Privacy Policy  </a> for the site.
+          </p>
+          <hr />
+          <p class="subtext">If you need more details please reach out to us again from <a target="_blank" style="color:#FF4057 !important" href="Contact.php">Get In Us </a> page. </p>
+        </div>
+        </div>
+    
+<?php
+include "footer.php";
+?>
